6c50d6869ed3e22e0bf40b08b80024f975ca648a
Enric Balletbo i Serra <eballetbo@gmail.com>
2012-07-25 17:15:48 +0200

    Add timings for Hynix H9DH1GH51JMPER series.
    Signed-off-by: Enric Balletbo i Serra <eballetbo@gmail.com>

--- src/memories/hynix.py
+++ src/memories/hynix.py
@@ -53,3 +53,54 @@ h9dh4ghxxjaper = [
          "wbst" : 0, "casl" : 3, "bl" : 2,
         },
     ]
+
+""" H9DH1GH51JMPER : SDRC parameters for a given SDRC clock rate
+
+Device summary:
+    H9DH1GH51JMPER series - NAND 1Gb(x16) / Mobile DDR 512Mb(x32)
+
+Datasheet : Are available from your local Hynix distributor
+ 
+"""
+h9dh1gh51jmper = [
+        { # 200 MHz
+         "tCK" : 5,
+         "tRFC" : 72, "tRC" : 40 + 15, "tRAS" : 40, "tRP" : 15, "tRCD" : 15,
+         "tRRD" : 10, "tWR" : 15, "tWTR" : 1, "tCKE" : 1,
+         "tXP" : 1, "tXSR" : 120,
+         "arcv" : (7.8 / 0.005) - 50, "are" : 1,
+         "wbst" : 0, "casl" : 3, "bl" : 2,
+        },
+        { # 185 MHz
+         "tCK" : 5.4,
+         "tRFC" : 72, "tRC" : 42 + 16.2, "tRAS" : 42, "tRP" : 16.2, "tRCD" : 16.2,
+         "tRRD" : 10.8, "tWR" : 15, "tWTR" : 1, "tCKE" : 1,
+         "tXP" : 1, "tXSR" : 120,
+         "arcv" : (7.8 / 0.005) - 50, "are" : 1,
+         "wbst" : 0, "casl" : 3, "bl" : 2,
+        },
+        { # 166 MHz
+         "tCK" : 6.06,
+         "tRFC" : 72, "tRC" : 42 + 18, "tRAS" : 42, "tRP" : 18, "tRCD" : 18,
+         "tRRD" : 12, "tWR" : 15, "tWTR" : 1, "tCKE" : 1,
+         "tXP" : 1, "tXSR" : 120,
+         "arcv" : (7.8 / 0.005) - 50, "are" : 1,
+         "wbst" : 0, "casl" : 3, "bl" : 2,
+        },
+        { # 133 MHz
+         "tCK" :  7.5,
+         "tRFC" : 72, "tRC" : 45 + 22.5, "tRAS" : 45, "tRP" : 22.5,
+         "tRCD" : 22.5, "tRRD" : 15, "tWR" : 15, "tWTR" : 1, "tCKE" : 1,
+         "tXP" : 1, "tXSR" : 120,
+         "arcv" : (7.8 / 0.005) - 50, "are" : 1,
+         "wbst" : 0, "casl" : 3, "bl" : 2,
+        },
+        { # 100 MHz
+         "tCK" : 10,
+         "tRFC" : 72, "tRC" : 50 + 30, "tRAS" : 50, "tRP" : 30, "tRCD" : 30,
+         "tRRD" : 15, "tWR" : 15, "tWTR" : 1, "tCKE" : 1,
+         "tXP" : 1, "tXSR" : 120,
+         "arcv" : (7.8 / 0.005) - 50, "are" : 1,
+         "wbst" : 0, "casl" : 3, "bl" : 2,
+        },
+    ]
--- src/timcalc.py
+++ src/timcalc.py
@@ -9,7 +9,8 @@ if __name__ == '__main__':
                  "M65Kx001AM" :  numonyx.m65kx001am,
                  "M65Kx002AM" :  numonyx.m65kx002am,
                  "MT29CxGxxMAxAPxxx" : micron.mt29cxgxxmaxapxxx,
-                 "H9DH4GHxxJAPER" : hynix.h9dh4ghxxjaper
+                 "H9DH4GHxxJAPER" : hynix.h9dh4ghxxjaper,
+                 "H9DH1GH51JMPER" : hynix.h9dh1gh51jmper
                 }
     # Print all timings
     for key in memories.keys():
